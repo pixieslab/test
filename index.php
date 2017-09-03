ebab29b984b703031e895b61506b0ab661d7f480
gabriele <gabriele.rizzo@pixieslab.com>
2017-09-03 13:56:31 +0200

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="it-IT">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,user-scalable=no">
+        <meta name="format-detection" content="telephone=no">
+        
+        <title>Arrè Gusto | Laboratorio sensoriale di vini e affini | Palermo</title>
+        <meta name="description" content="Arrè Gusto, laboratorio sensoriale di vini e affini, Palermo.">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <!--[if lt IE 9]>
+        <script src="js/html5.js"></script>
+        <![endif]-->
+
+        <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
+    </head>
+
+    <body>
+        <div id="site">
+            <a id="logo" href="mailto:info@arregusto.it"><img src="images/arregusto.png"/></a>
+            <a id="instagram" href="https://www.instagram.com/arregusto/" target="_blank">Follow us on 2<img src="images/instagram.png"/></a>
+        </div>
+    </body>
+</html>
+
+
